ae5d409ebee840c1e482692bc34d5376928b52c5
chandradewi26 <69431090+chandradewi26@users.noreply.github.com>
2022-08-09 02:31:12 +1000

    Initial structure of CreditScore App and CreditScore Tests

---
+++ credit-score-test/CreditScore.Tests/CreditScore_IsTest.cs
@@ -0,0 +1,10 @@
+namespace CreditScore.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ credit-score-test/CreditScore/CreditScore.cs
@@ -0,0 +1,5 @@
+﻿namespace CreditScore;
+public class Class1
+{
+
+}
