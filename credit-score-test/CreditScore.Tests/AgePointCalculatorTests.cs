2a403b7bc498927ea8a7f6c84a07ff7dcd0a830c
chandradewi26 <69431090+chandradewi26@users.noreply.github.com>
2022-08-09 22:07:54 +1000

    AgePointCalculator revise code logic & change test method names

--- credit-score-test/CreditScore.Tests/AgePointCalculatorTests.cs
+++ credit-score-test/CreditScore.Tests/AgePointCalculatorTests.cs
@@ -12,7 +12,7 @@ namespace CreditScore.Tests
         }
 
         [Fact(DisplayName = "The 'AgePointCalculator' should return 0 points from the given age of negative number (-10)")]
-        public void TestPointCalculation_InputIsNegativeNumber_ExpectedPoints0()
+        public void TestPointCalculation_AgeIsNegativeNumber_ExpectedPoints0()
         {
             //Act
             int points = _calculator.CalculatePoint(-10);
@@ -20,7 +20,7 @@ namespace CreditScore.Tests
             Assert.StrictEqual(0, points);
         }
         [Fact(DisplayName = "The 'AgePointCalculator' should return 0 points from the given age of younger than 18 years old")]
-        public void TestPointCalculation_InputLessThan18_ExpectedPoints0()
+        public void TestPointCalculation_AgeIsLessThan18_ExpectedPoints0()
         {
             //Act
             int points = _calculator.CalculatePoint(17);
@@ -29,7 +29,7 @@ namespace CreditScore.Tests
         }
 
         [Fact(DisplayName = "The 'AgePointCalculator' should return 3 points from the given age range of 18 to 25 years old")]
-        public void TestPointCalculation_InputBetween18to25_ExpectedPoints3()
+        public void TestPointCalculation_AgeIsBetween18to25_ExpectedPoints3()
         {
             //Act
             int points = _calculator.CalculatePoint(18);
@@ -38,7 +38,7 @@ namespace CreditScore.Tests
         }
 
         [Fact(DisplayName = "The 'AgePointCalculator' should return 4 points from the given age range of 26 to 35 years old")]
-        public void TestPointCalculation_InputBetween26to35_ExpectedPoints4()
+        public void TestPointCalculation_AgeIsBetween26to35_ExpectedPoints4()
         {
             //Act
             int points = _calculator.CalculatePoint(26);
@@ -47,16 +47,16 @@ namespace CreditScore.Tests
         }
 
         [Fact(DisplayName = "The 'AgePointCalculator' should return 5 points from the given age range of 36 to 50 years old")]
-        public void TestPointCalculation_InputBetween36to50_ExpectedPoints5()
+        public void TestPointCalculation_AgeIsBetween36to50_ExpectedPoints5()
         {
             //Act
-            int points = _calculator.CalculatePoint(36);
+            int points = _calculator.CalculatePoint(50);
             //Assert (expected, actual)
             Assert.StrictEqual(5, points);
         }
 
         [Fact(DisplayName = "The 'AgePointCalculator' should return 6 points from the given age of more than 51 years old")]
-        public void TestPointCalculation_InputIsMoreThan51_ExpectedPoints6()
+        public void TestPointCalculation_AgeIsMoreThan51_ExpectedPoints6()
         {
             //Act
             int points = _calculator.CalculatePoint(51);
--- credit-score-test/CreditScore/AgePointCalculator.cs
+++ credit-score-test/CreditScore/AgePointCalculator.cs
@@ -6,11 +6,11 @@
         {
             if (age < 18)
                 return 0;
-            if (age >= 18 && age < 25)
+            if (age >= 18 && age <= 25)
                 return 3;
-            if (age >= 26 && age < 35)
+            if (age >= 26 && age <= 35)
                 return 4;
-            if (age >= 36 && age < 50)
+            if (age >= 36 && age <= 50)
                 return 5;
             if (age >= 51)
                 return 6;
