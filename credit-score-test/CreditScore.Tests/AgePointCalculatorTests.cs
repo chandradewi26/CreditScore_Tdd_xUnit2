3ffb8a8d37d8ea548db4005f7b0ee7fee63f0658
chandradewi26 <69431090+chandradewi26@users.noreply.github.com>
2022-08-09 20:44:24 +1000

    TDD-style : AgePointCalculator class and tests

--- credit-score-test/CreditScore.Tests/AgePointCalculatorTests.cs
+++ credit-score-test/CreditScore.Tests/AgePointCalculatorTests.cs
@@ -2,6 +2,7 @@ namespace CreditScore.Tests
 {
     public class AgePointCalculatorTests
     {
+        //Arrange
         private readonly AgePointCalculator _calculator;
 
         public AgePointCalculatorTests()
@@ -10,44 +11,52 @@ namespace CreditScore.Tests
             _calculator = new AgePointCalculator();
         }
 
-        [Fact(DisplayName = "The 'age point calculator' should return 0 points from the given age of 15")]
-        public void TestPointCalculation_InputIs15_ExpectedIs0()
+        [Fact(DisplayName = "The 'AgePointCalculator' should return 0 points from the given age of negative number (-10)")]
+        public void TestPointCalculation_InputIs65_ExpectedPoints6()
         {
             //Act
-            int points = _calculator.CalculatePoint(15);
+            int points = _calculator.CalculatePoint(-10);
+            //Assert (expected, actual)
+            Assert.StrictEqual(0, points);
+        }
+        [Fact(DisplayName = "The 'AgePointCalculator' should return 0 points from the given age of younger than 18 years old")]
+        public void TestPointCalculation_InputLessThan18_ExpectedPoints0()
+        {
+            //Act
+            int points = _calculator.CalculatePoint(17);
             //Assert (expected, actual)
             Assert.StrictEqual(0, points);
         }
 
-        [Fact(DisplayName = "The 'age point calculator' should return 3 points from the given age of 25")]
-        public void TestPointCalculation_InputIs25_ExpectedIs3()
+        [Fact(DisplayName = "The 'AgePointCalculator' should return 3 points from the given age range of 18 to 25 years old")]
+        public void TestPointCalculation_InputBetween18to25_ExpectedPoints3()
         {
             //Act
-            int points = _calculator.CalculatePoint(25);
+            int points = _calculator.CalculatePoint(18);
             //Assert (expected, actual)
             Assert.StrictEqual(3, points);
         }
 
-        [Fact(DisplayName = "The 'age point calculator' should return 4 points from the given age of 35")]
-        public void TestPointCalculation_InputIs35_ExpectedIs4()
+        [Fact(DisplayName = "The 'AgePointCalculator' should return 4 points from the given age range of 26 to 35 years old")]
+        public void TestPointCalculation_InputBetween26to35_ExpectedPoints4()
         {
             //Act
-            int points = _calculator.CalculatePoint(35);
+            int points = _calculator.CalculatePoint(26);
             //Assert (expected, actual)
             Assert.StrictEqual(4, points);
         }
 
-        [Fact(DisplayName = "The 'age point calculator' should return 5 points from the given age of 50")]
-        public void TestPointCalculation_InputIs50_ExpectedIs5()
+        [Fact(DisplayName = "The 'AgePointCalculator' should return 5 points from the given age range of 36 to 50 years old")]
+        public void TestPointCalculation_InputBetween36to50_ExpectedPoints5()
         {
             //Act
-            int points = _calculator.CalculatePoint(50);
+            int points = _calculator.CalculatePoint(36);
             //Assert (expected, actual)
             Assert.StrictEqual(5, points);
         }
 
-        [Fact(DisplayName = "The 'age point calculator' should return 6 points from the given age of 51")]
-        public void TestPointCalculation_InputIs51_ExpectedIs6()
+        [Fact(DisplayName = "The 'AgePointCalculator' should return 6 points from the given age of more than 51 years old")]
+        public void TestPointCalculation_InputIsMoreThan51_ExpectedPoints6()
         {
             //Act
             int points = _calculator.CalculatePoint(51);
--- credit-score-test/CreditScore/AgePointCalculator.cs
+++ credit-score-test/CreditScore/AgePointCalculator.cs
@@ -4,6 +4,17 @@
     {
         public int CalculatePoint(int age)
         {
+            if (age < 18)
+                return 0;
+            if (age >= 18 && age < 25)
+                return 3;
+            if (age >= 26 && age < 35)
+                return 4;
+            if (age >= 36 && age < 50)
+                return 5;
+            if (age >= 51)
+                return 6;
+                
             throw new NotImplementedException();
         }
     }
