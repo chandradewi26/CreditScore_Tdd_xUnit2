4d3aa33fa535d210666ca4a3de7d65adf3b5a389
chandradewi26 <69431090+chandradewi26@users.noreply.github.com>
2022-08-09 22:16:07 +1000

    CompletedPaymentCalculator class and tests

--- credit-score-test/CreditScore.Tests/CompletedPaymentCalculatorTests.cs
+++ credit-score-test/CreditScore.Tests/CompletedPaymentCalculatorTests.cs
@@ -10,38 +10,52 @@ namespace CreditScore.Tests
             _calculator = new CompletedPaymentCalculator();
         }
 
-        [Fact(DisplayName = "The 'completed payment point calculator' should return 0 points from the given completed payment count of 0")]
-        public void TestPointCalculation_InputIs0_ExpectedIs0()
+        [Fact(DisplayName = "The 'CompletedPaymentCalculator' should return 0 points from the given completed payment count of negative numbers")]
+        public void TestPointCalculation_CompletedPaymentCountIsNegativeNumber10_ExpectedPointIs0()
+        {
+            //Act
+            int points = _calculator.CalculatePoint(-10);
+            //Assert (expected, actual)
+            Assert.StrictEqual(0, points);
+        }
+
+        [Fact(DisplayName = "The 'CompletedPaymentCalculator' should return 0 points from the given completed payment count of 0")]
+        public void TestPointCalculation_CompletedPaymentCountIs0_ExpectedPointIs0()
         {
             //Act
             int points = _calculator.CalculatePoint(0);
             //Assert (expected, actual)
             Assert.StrictEqual(0, points);
         }
-
-        [Fact(DisplayName = "The 'completed payment point calculator' should return 2 points from the given completed payment count of 1")]
-        public void TestPointCalculation_InputIs1_ExpectedIs2()
+        [Fact(DisplayName = "The 'CompletedPaymentCalculator' should return 2 points from the given completed payment count of 1")]
+        public void TestPointCalculation_CompletedPaymentCountIs1_ExpectedPointIs2()
         {
             //Act
             int points = _calculator.CalculatePoint(1);
             //Assert (expected, actual)
             Assert.StrictEqual(2, points);
         }
-
-        [Fact(DisplayName = "The 'completed payment point calculator' should return 3 points from the given completed payment count of 2")]
-        public void TestPointCalculation_InputIs2_ExpectedIs3()
+        [Fact(DisplayName = "The 'CompletedPaymentCalculator' should return 3 points from the given completed payment count of 2")]
+        public void TestPointCalculation_CompletedPaymentCountIs2_ExpectedPointIs3()
         {
             //Act
             int points = _calculator.CalculatePoint(2);
             //Assert (expected, actual)
             Assert.StrictEqual(3, points);
         }
-
-        [Fact(DisplayName = "The 'completed payment point calculator' should return 4 points from the given completed payment count of 3")]
-        public void TestPointCalculation_InputIs3_ExpectedIs4()
+        [Fact(DisplayName = "The 'CompletedPaymentCalculator' should return 4 points from the given completed payment count of 3")]
+        public void TestPointCalculation_CompletedPaymentCountIs3_ExpectedPointIs4()
         {
             //Act
             int points = _calculator.CalculatePoint(3);
+            //Assert (expected, actual)
+            Assert.StrictEqual(4, points);
+        }
+        [Fact(DisplayName = "The 'CompletedPaymentCalculator' should return 4 points from the given completed payment count of more than 3")]
+        public void TestPointCalculation_CompletedPaymentCountIs5_ExpectedPointIs4()
+        {
+            //Act
+            int points = _calculator.CalculatePoint(5);
             //Assert (expected, actual)
             Assert.StrictEqual(4, points);
         }
--- credit-score-test/CreditScore/CompletedPaymentCalculator.cs
+++ credit-score-test/CreditScore/CompletedPaymentCalculator.cs
@@ -4,6 +4,18 @@
     {
         public int CalculatePoint(int completedPayment)
         {
+            if (completedPayment < 0)
+                return 0;
+            if (completedPayment == 0)
+                return 0;
+            if (completedPayment == 1)
+                return 2;
+            if (completedPayment == 2)
+                return 3;
+            if (completedPayment == 3)
+                return 4;
+            if (completedPayment > 3)
+                return 4;
             throw new NotImplementedException();
         }
     }
