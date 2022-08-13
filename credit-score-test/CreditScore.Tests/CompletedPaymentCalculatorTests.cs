fe0776795b0db4410adfda0761a4a82afe1b594c
chandradewi26 <69431090+chandradewi26@users.noreply.github.com>
2022-08-14 06:13:53 +1000

    CompletedPaymentCalculator Class and Tests -Update2

--- credit-score-test/CreditScore.Tests/CompletedPaymentCalculatorTests.cs
+++ credit-score-test/CreditScore.Tests/CompletedPaymentCalculatorTests.cs
@@ -10,52 +10,38 @@ namespace CreditScore.Tests
             _calculator = new CompletedPaymentCalculator();
         }
 
-        [Fact(DisplayName = "The 'CompletedPaymentCalculator' should return 0 points from the given completed payment count of negative numbers")]
-        public void TestPointCalculation_CompletedPaymentCountIsNegativeNumber10_ExpectedPointIs0()
-        {
-            //Act
-            int points = _calculator.CalculatePoint(-10);
-            //Assert (expected, actual)
-            Assert.StrictEqual(0, points);
-        }
-
-        [Fact(DisplayName = "The 'CompletedPaymentCalculator' should return 0 points from the given completed payment count of 0")]
-        public void TestPointCalculation_CompletedPaymentCountIs0_ExpectedPointIs0()
+        [Fact(DisplayName = "When the customer has 0 completed payment count, CompletedPaymentCalculator should return 0 point")]
+        public void TestPointCalculation_CompPaymentCount0_Return0()
         {
             //Act
             int points = _calculator.CalculatePoint(0);
             //Assert (expected, actual)
             Assert.StrictEqual(0, points);
         }
-        [Fact(DisplayName = "The 'CompletedPaymentCalculator' should return 2 points from the given completed payment count of 1")]
-        public void TestPointCalculation_CompletedPaymentCountIs1_ExpectedPointIs2()
+
+        [Fact(DisplayName = "When the customer has 1 completed payment count, CompletedPaymentCalculator should return 2 points")]
+        public void TestPointCalculation_CompPaymentCount1_Return2()
         {
             //Act
             int points = _calculator.CalculatePoint(1);
             //Assert (expected, actual)
             Assert.StrictEqual(2, points);
         }
-        [Fact(DisplayName = "The 'CompletedPaymentCalculator' should return 3 points from the given completed payment count of 2")]
-        public void TestPointCalculation_CompletedPaymentCountIs2_ExpectedPointIs3()
+
+        [Fact(DisplayName = "When the customer has 2 completed payment count, CompletedPaymentCalculator should return 3 points")]
+        public void TestPointCalculation_CompPaymentCount2_Return3()
         {
             //Act
             int points = _calculator.CalculatePoint(2);
             //Assert (expected, actual)
             Assert.StrictEqual(3, points);
         }
-        [Fact(DisplayName = "The 'CompletedPaymentCalculator' should return 4 points from the given completed payment count of 3")]
-        public void TestPointCalculation_CompletedPaymentCountIs3_ExpectedPointIs4()
+
+        [Fact(DisplayName = "When the customer has equal to or more than 3 completed payment count, CompletedPaymentCalculator should return 4 points")]
+        public void TestPointCalculation_CompPaymentCountEqualOrMoreThan3_Return4()
         {
             //Act
             int points = _calculator.CalculatePoint(3);
-            //Assert (expected, actual)
-            Assert.StrictEqual(4, points);
-        }
-        [Fact(DisplayName = "The 'CompletedPaymentCalculator' should return 4 points from the given completed payment count of more than 3")]
-        public void TestPointCalculation_CompletedPaymentCountIs5_ExpectedPointIs4()
-        {
-            //Act
-            int points = _calculator.CalculatePoint(5);
             //Assert (expected, actual)
             Assert.StrictEqual(4, points);
         }
