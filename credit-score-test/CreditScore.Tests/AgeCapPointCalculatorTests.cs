c005133560151a5b97efe22893a6dc46253bfc1e
chandradewi26 <69431090+chandradewi26@users.noreply.github.com>
2022-08-16 15:26:22 +1000

    Refactor AgeCapPointCalculator Tests

--- credit-score-test/CreditScore.Tests/AgeCapPointCalculatorTests.cs
+++ credit-score-test/CreditScore.Tests/AgeCapPointCalculatorTests.cs
@@ -12,29 +12,25 @@ namespace CreditScore.Tests
         [Fact(DisplayName = "When a customer is 18~25 years old, AgeCapPointCalculator should return maximum points of 3")]
         public void TestPointCalculation_Age18to25_Return3()
         {
-            var points = _calculator.CalculatePoint(18);
-            Assert.StrictEqual(3, points);
+            TestPointCalculation(18, 3);
         }
 
         [Fact(DisplayName = "When a customer is 26~35 years old, AgeCapPointCalculator should return maximum points of 4")]
         public void TestPointCalculation_Age26to35_Return4()
         {
-            var points = _calculator.CalculatePoint(26);
-            Assert.StrictEqual(4, points);
+            TestPointCalculation(26, 4);
         }
 
         [Fact(DisplayName = "When a customer is 36~50 years old, AgeCapPointCalculator should return maximum points of 5")]
         public void TestPointCalculation_Age36to50_Return5()
         {
-            var points = _calculator.CalculatePoint(36);
-            Assert.StrictEqual(5, points);
+            TestPointCalculation(36,5);
         }
 
         [Fact(DisplayName = "When a customer is at age or older than 51 years old, AgeCapPointCalculator should return maximum points of 6")]
         public void TestPointCalculation_Age51OrOlder_Return6()
         {
-            var points = _calculator.CalculatePoint(51);
-            Assert.StrictEqual(6, points);
+            TestPointCalculation(51,6);
         }
 
         [Fact(DisplayName = "When a customer is younger than 18 years old, AgeCapPointCalculator should throw exception")]
@@ -42,6 +38,12 @@ namespace CreditScore.Tests
         {
             Action action = () => _calculator.CalculatePoint(10);
             Assert.Throws<ArgumentOutOfRangeException>(action);
+        }
+
+        public void TestPointCalculation(int inputValue, int expectedOutput)
+        {
+            var points = _calculator.CalculatePoint(inputValue);
+            Assert.StrictEqual(expectedOutput, points);
         }
     }
 }
