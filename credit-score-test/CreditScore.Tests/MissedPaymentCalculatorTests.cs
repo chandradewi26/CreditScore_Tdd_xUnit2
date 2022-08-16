986e4f1d68bf25dbf19361b1136731ff176e748d
chandradewi26 <69431090+chandradewi26@users.noreply.github.com>
2022-08-16 15:38:21 +1000

    Refactor MissedPaymentCalculator Tests

--- credit-score-test/CreditScore.Tests/MissedPaymentCalculatorTests.cs
+++ credit-score-test/CreditScore.Tests/MissedPaymentCalculatorTests.cs
@@ -12,29 +12,25 @@ namespace CreditScore.Tests
         [Fact(DisplayName = "When the customer has 0 missed payment count, MissedPaymentCalculator should return 0 point")]
         public void TestPointCalculation_MissedPaymentCount0_Return0()
         {
-            var points = _calculator.CalculatePoint(0);
-            Assert.StrictEqual(0, points);
+            TestPointCalculation(0, 0);
         }
 
         [Fact(DisplayName = "When the customer has 1 missed payment count, MissedPaymentCalculator should return -1 point")]
         public void TestPointCalculation_MissedPaymentCount1_ReturnMin1()
         {
-            var points = _calculator.CalculatePoint(1);
-            Assert.StrictEqual(-1, points);
+            TestPointCalculation(1, -1);
         }
 
         [Fact(DisplayName = "When the customer has 2 missed payment counts, MissedPaymentCalculator should return -3 points")]
         public void TestPointCalculation_MissedPaymentCount2_ReturnMin3()
         {
-            var points = _calculator.CalculatePoint(2);
-            Assert.StrictEqual(-3, points);
+            TestPointCalculation(2, -3);
         }
 
         [Fact(DisplayName = "When the customer has equal to or more than 3 missed payment counts, MissedPaymentCalculator should return -6 points")]
         public void TestPointCalculation_MissedPaymentCountMoreEqualOrMoreThan3_ReturnMin6()
         {
-            var points = _calculator.CalculatePoint(5);
-            Assert.StrictEqual(-6, points);
+            TestPointCalculation(5, -6);
         }
 
         [Fact(DisplayName = "Given negative number of missed payment count, MissedPaymentCalculator should throw exception")]
@@ -42,6 +38,12 @@ namespace CreditScore.Tests
         {
             Action action = () => _calculator.CalculatePoint(-5);
             ArgumentOutOfRangeException exception = Assert.Throws<ArgumentOutOfRangeException>(action);
+        }
+
+        public void TestPointCalculation(int inputValue, int expectedOutput)
+        {
+            var points = _calculator.CalculatePoint(inputValue);
+            Assert.StrictEqual(expectedOutput, points);
         }
     }
 }
