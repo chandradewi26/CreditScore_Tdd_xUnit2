10ef72b4feef0a1d56891373337ff97d17598aef
chandradewi26 <69431090+chandradewi26@users.noreply.github.com>
2022-08-16 15:28:37 +1000

    Refactor BureauScoreCalculator Tests

--- credit-score-test/CreditScore.Tests/BureauScoreCalculatorTests.cs
+++ credit-score-test/CreditScore.Tests/BureauScoreCalculatorTests.cs
@@ -12,29 +12,25 @@ namespace CreditScore.Tests
         [Fact(DisplayName = "Given 451 to 700 credit bureau, BureauScoreCalculator should return 1 point")]
         public void TestPointCalculation_BScore451to700_Return1Point()
         {
-            var points = _calculator.CalculatePoint(451);
-            Assert.StrictEqual(1, points);
+            TestPointCalculation(451, 1);
         }
 
         [Fact(DisplayName = "Given 701 to 850 credit bureau, BureauScoreCalculator should return 2 point")]
         public void TestPointCalculation_BScore701to850_Return2Point()
         {
-            var points = _calculator.CalculatePoint(701);
-            Assert.StrictEqual(2, points);
+            TestPointCalculation(701, 2);
         }
 
         [Fact(DisplayName = "Given 851 to 1000 credit bureau, BureauScoreCalculator should return 3 point")]
         public void TestPointCalculation_BScore851to1000_Return3Point()
         {
-            var points = _calculator.CalculatePoint(851);
-            Assert.StrictEqual(3, points);
+            TestPointCalculation(851, 3);
         }
 
         [Fact(DisplayName = "Given more than 1000 credit bureau, BureauScoreCalculator should return highest possible point (3)")]
         public void TestPointCalculation_BScoreMoreThan1000_Return3Point()
         {
-            var points = _calculator.CalculatePoint(1001);
-            Assert.StrictEqual(3, points);
+            TestPointCalculation(1001, 3);
         }
 
         [Fact(DisplayName = "Given less than 450 credit bureau, BureauScoreCalculator should throw exception and the customer is not qualified")]
@@ -42,6 +38,12 @@ namespace CreditScore.Tests
         {
             Action action = () => _calculator.CalculatePoint(50);
             Assert.Throws<ArgumentOutOfRangeException>(action);
+        }
+
+        public void TestPointCalculation(int inputValue, int expectedOutput)
+        {
+            var points = _calculator.CalculatePoint(inputValue);
+            Assert.StrictEqual(expectedOutput, points);
         }
     }
 }
