33c891c101f7e88d3b198aa38e992eeb48f172b1
chandradewi26 <69431090+chandradewi26@users.noreply.github.com>
2022-08-29 11:16:18 +1000

    Removed comments

--- credit-score-test/CreditScore.Tests/ZipCreditCalculatorTests.cs
+++ credit-score-test/CreditScore.Tests/ZipCreditCalculatorTests.cs
@@ -8,23 +8,19 @@ namespace CreditScore.Tests
 
         public ZipCreditCalculatorTests()
         {
-            //1. We need a service collection
             var collection = new ServiceCollection();
             collection.AddScoped<IAgeCapPointCalculator, AgeCapPointCalculator>();
             collection.AddScoped<IBureauScoreCalculator, BureauScoreCalculator>();
             collection.AddScoped<IMissedPaymentCalculator, MissedPaymentCalculator>();
             collection.AddScoped<ICompletedPaymentCalculator, CompletedPaymentCalculator>();
 
-            //2.Instance of service provider
             var provider = collection.BuildServiceProvider();
 
-            //3.IBusiness biz = provider.GetService<IBusiness>();
             IAgeCapPointCalculator ageCapPointCalculator = provider.GetService<IAgeCapPointCalculator>();
             IBureauScoreCalculator bureauScoreCalculator = provider.GetService<IBureauScoreCalculator>();
             IMissedPaymentCalculator missedPaymentCalculator = provider.GetService<IMissedPaymentCalculator>();
             ICompletedPaymentCalculator completedPaymentCalculator = provider.GetService<ICompletedPaymentCalculator>();
 
-            //4. var userInterface = new UserInterface(biz);
             _calculator = new ZipCreditCalculator(ageCapPointCalculator, bureauScoreCalculator, missedPaymentCalculator, completedPaymentCalculator);
         }
         
