d47a05653569a683889bde00cdc9156f5ec8e851
chandradewi26 <69431090+chandradewi26@users.noreply.github.com>
2022-09-07 03:11:21 +1000

    PointScore/NotEligible return type for result

---
+++ credit-score-test/CreditScore/PointScore.cs
@@ -0,0 +1,14 @@
+﻿namespace CreditScore
+{
+    //What is sealed?
+    //public sealed class PointScore : IPointCalculationResult
+    public class PointScore : IPointsCalculationResult
+    {
+        public int Points { get; }
+
+        public PointScore (int points)
+        {
+            Points = points;
+        }   
+    }
+}
